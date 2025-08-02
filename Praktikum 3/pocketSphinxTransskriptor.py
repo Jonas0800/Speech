c384120b6ccd6fdc5afa3deb676792ebcab2bb36
Jonas0800 <jonas.krumeich@gmail.com>
2025-08-02 18:06:41 +0200

    Update and rename main.py to pocketSphinxTransskriptor.py

--- Praktikum 3/pocketSphinxTransskriptor.py
+++ Praktikum 3/pocketSphinxTransskriptor.py
@@ -5,9 +5,9 @@ audio_path = r"Timeline 1.wav"
 
 
 config = Config()
-config.set_string('-hmm', r'C:\Users\Jonas\PycharmProjects\Speech\.venv\Lib\site-packages\pocketsphinx\model\De-de\cmusphinx-cont-voxforge-de-r20171217\model_parameters\voxforge.cd_cont_6000')
-config.set_string('-dict', r'C:\Users\Jonas\PycharmProjects\Speech\.venv\Lib\site-packages\pocketsphinx\model\De-de\cmusphinx-voxforge-de.dic')
-config.set_string('-lm', r'C:\Users\Jonas\PycharmProjects\Speech\.venv\Lib\site-packages\pocketsphinx\model\De-de\cmusphinx-voxforge-de.lm.bin')
+config.set_string('-hmm', r'Speech\.venv\Lib\site-packages\pocketsphinx\model\De-de\cmusphinx-cont-voxforge-de-r20171217\model_parameters\voxforge.cd_cont_6000')
+config.set_string('-dict', r'\Speech\.venv\Lib\site-packages\pocketsphinx\model\De-de\cmusphinx-voxforge-de.dic')
+config.set_string('-lm', r'C:\Speech\.venv\Lib\site-packages\pocketsphinx\model\De-de\cmusphinx-voxforge-de.lm.bin')
 
 
 decoder = Decoder(config)
@@ -28,3 +28,4 @@ if hypothesis is not None:
     print("Transkription:", hypothesis.hypstr)
 else:
     print("Keine Sprache erkannt.")
+
